4116a39bed747357a7b14f37bed1c6b1fa7ba947
owlbytes <neshah07@gmail.com>
2014-06-18 20:37:06 +0100

    added hash to weight difficulty

--- tutorials.rb
+++ tutorials.rb
@@ -1,9 +1,11 @@
 
+
 class Tutorial
   def initialize(name, type, difficulty)
     @name = name
     @type = type
     @difficulty = difficulty
+    @weight = {:easy => 1, :medium => 2, :hard => 3}
   end
 
   def type_comparison(tutorial)
@@ -15,7 +17,7 @@ class Tutorial
   end
 
   def is_harder_than?(tutorial)
-    if @difficulty > tutorial.difficulty
+    if @weight[@difficulty] > @weight[tutorial.difficulty]
       puts "true" 
     else
       puts "false"
@@ -31,10 +33,16 @@ class Tutorial
   end
 end
 
-tutorial1 = Tutorial.new("Object Oriented Ruby", :ruby, 2)
-turorial2 = Tutorial.new("Introduction to JavaScript", :javascript, 1)
-tutorial3 = Tutorial.new("Introduction to JavaScript", :javascript, 4)
+tutorial1 = Tutorial.new("Object Oriented Ruby", :ruby, :easy)
+turorial2 = Tutorial.new("Introduction to JavaScript", :javascript, :medium)
+tutorial3 = Tutorial.new("Introduction to JavaScript 2", :javascript, :medium)
+tutorial4 = Tutorial.new("Introduction to JavaScript 2", :javascript, :hard)
 
+#cannot compare
 tutorial1.type_comparison(turorial2)
 
-turorial2.type_comparison(tutorial3)
+#2 is harder than 3
+tutorial4.type_comparison(turorial2)
+
+#3 is not harder than 4
+tutorial3.type_comparison(tutorial4)
